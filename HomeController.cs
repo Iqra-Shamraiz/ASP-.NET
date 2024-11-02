904684740216a3e43f2443b21509b8cdde3d01fa
Iqra Shamraiz <Learning4402823@gmail.com>
2024-11-02 03:33:51 -0700

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MapRouting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
